260ce316e813435a8034fd8e67eaa9b4c81cf0f0
Satyaarth Chhabra <satyarthchhabra18@gmail.com>
2021-08-20 13:16:28 +0530

    project style changed

--- src/components/home/Project.jsx
+++ src/components/home/Project.jsx
@@ -1,22 +1,29 @@
 import React from "react";
-import { Container, Jumbotron, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Jumbotron, Row } from "react-bootstrap";
+import YouTube from 'react-youtube'
 import projectData from "../../projectData/projectData";
 import ProjectCard from "./ProjectCard";
 
 const Project = () => {
+
   return (
     <Jumbotron fluid id="projects" className="bg-light m-0">
       <Container className="">
         <h2 className=" mb-5 text-center name-text  text-new-style">
           Projects
         </h2>
-        <Row>
-          {/* {
+
+        <Row className="project__container justify-content-between mx-auto">
+
+
+
+
+          {
             projectData?.map(project => (
 
               <ProjectCard heading={project.heading} techStacks={project.techStacks} features={project.features} overview={project.overview} liveSiteURL={project.liveSiteURL} videoId={project.videoId} sourceCodeURL={project.sourceCodeURL} />
             ))
-          } */}
+          }
         </Row>
       </Container>
     </Jumbotron>
--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -1,21 +1,68 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react'
-import { Button, Col, Row } from 'react-bootstrap'
+
+import React, { useState } from 'react'
+import { Modal, Button, Card, Col, Row } from 'react-bootstrap'
 import YouTube from 'react-youtube'
+import useWindowDimensions from '../../hooks/useWindowDimension'
 import "./ProjectCard.css"
-const ProjectCard = ({liveSiteURL,videoId, heading, overview, techStacks,features,sourceCodeURL }) => {
+const ProjectCard = ({ liveSiteURL, videoId, heading, overview, techStacks, features, sourceCodeURL }) => {
+    const { height, width } = useWindowDimensions();
+    console.log(height, width);
+    const [modalShow, setModalShow] = useState(false)
     const opts = {
-        height: '390',
-        width: '640',
+        height: '250',
+        width: '340',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             //   autoplay: 1,
         },
     };
-    
     return (
         <>
-            <Col xs={12} className="py-4">
+            <Col md={4} xs={10} className="my-4 mx-auto " >
+                <Card className="border-0 py-2">
+                    <YouTube containerClassName={"youtubeContainer"} videoId={videoId}
+                        opts={opts} />
+                    <Card.Body className="text-center"  >
+                        <Card.Title  >{heading} </Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="" onClick={() => {
+                            window.open(sourceCodeURL)
+                        }} className="rounded-circle"><i class="fab fa-github"></i></Button>
+                        <Button variant="light" onClick={() => setModalShow(true)} className="mx-2 project-center-btn">Check Details</Button>
+                        <Button variant="" onClick={() => {
+                            window.open(liveSiteURL)
+                        }} className="rounded-circle" ><i class="far fa-share-square"></i></Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Modal
+                show={modalShow}
+                size="lg"
+                onHide={() => setModalShow(false)}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Modal heading
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Centered Modal</h4>
+                    <p>
+                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                        consectetur ac, vestibulum at eros.
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={() => setModalShow(false)}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+            {/* <Col xs={12} className="py-4">
                 <Row>
 
                     <Col xs={12} sm={6}>
@@ -48,19 +95,19 @@ const ProjectCard = ({liveSiteURL,videoId, heading, overview, techStacks,feature
                                             <li key={`${stack}-${index}`}>{stack}</li>
                                         ))
                                     }
-                                   
+
                                 </ul>
                             </Col>
                         </Row>
 
                         <Row className="justify-content-center" >
                             <Col xs={12} sm={6} className="my-2">
-                                <Button variant="outline-secondary" onClick={()=>{
+                                <Button variant="outline-secondary" onClick={() => {
                                     window.open(sourceCodeURL)
-                                }}  block>🔀 Source Code</Button>
+                                }} block>🔀 Source Code</Button>
                             </Col>
                             <Col xs={12} sm={6} className="my-2">
-                                <Button variant="outline-success" onClick={()=>{
+                                <Button variant="outline-success" onClick={() => {
                                     window.open(liveSiteURL)
                                 }} block>Live Site 👋🏻 </Button>
 
@@ -72,8 +119,8 @@ const ProjectCard = ({liveSiteURL,videoId, heading, overview, techStacks,feature
                             opts={opts} />
                     </Col>
                 </Row>
-<h6 className="text-muted text-center"> the live site for above project may take few time to load as it is hosted on free hosting  🥱</h6>
-            </Col>
+                <h6 className="text-muted text-center"> the live site for above project may take few time to load as it is hosted on free hosting  🥱</h6>
+            </Col> */}
 
         </>
     )
