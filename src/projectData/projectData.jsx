722294c6516272bff8b6d22b16effc722527cd30
satyaarth chhabra <satyarthchhabra18@gmail.com>
2022-10-02 19:40:38 +0530

    new product added

--- src/projectData/projectData.jsx
+++ src/projectData/projectData.jsx
@@ -1,145 +1,160 @@
 const projectData = [
-    {
-        heading: "Amazon Clone",
-        overview: "ecommerce site with all neccessary features like admin dashboard and seller dashboard having backed with firebase and payments through stripe",
-        features: [
-            "multi paged website",
-            "responsive design",
-            "stripe payments",
-            "login functionality",
-            "login with google",
-            "automatic city and state detection system",
-            "profile upgardation "
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "material-ui",
-            "firebase",
-            "stripe.js",
-            "axios",
-            "faker",
-            "moment",
-            "react-currency-format",
-            "react-flip-move",
-            "uuid",
-            "toastify",
-        ],
-        liveSiteURL: 'https://satya-amazon-clone.netlify.app',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/amazon-clone-firebase",
-        videoId: "MthDpsxcDGc"
-    },
-    {
-        heading: "Netflix Clone",
-        overview: "Built with react, this site is an media streaming site with backend in firebase  ",
-        features: [
-            "multi paged website",
-            "responsive design",
-            "search functionality",
-            "login functionality",
-            "login with google",
-            "youtube-video for the content will be played "
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "react-youtube",
-            "firebase",
-            "normalize.css",
-            "fuse.js",
-            "styled-components",
-        ],
-        liveSiteURL: 'https://satya-netflix-clone.netlify.app',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/netflix-clone-firebase-auth",
-        videoId: "mymuAhxSMN8"
-    },
-    {
-        heading: "World Corona Monitor",
-        overview: "Displays covid-19 status in different countries through maps,graphs and lists",
-        features: [
-            "Daily covid cases",
-            "Map representations for cases",
-            "Graph representations for cases",
-            "Each Country alone representation also available",
-
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "axios",
-            "react-chartjs",
-            "leaflet",
-            "numeral",
-            "react-charjs-2",
-        ],
-        liveSiteURL: 'https://satya-corona-go.netlify.app',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/go-corona/",
-        videoId: "Kkd8v80SsOg"
-    },
-    {
-
-        heading: "Meme Wala",
-        overview: "It contains over 150 meme template and can help you to create meme",
-        features: [
-            "aprox 200 meme template",
-            "easy process",
-            "fast functionality",
-            "download functionality available ",
-
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "axios",
-            "react-bootstrap",
-            "bootstrap",
-        ],
-        liveSiteURL: 'https://meme-wala.netlify.app',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/meme-wala/",
-        videoId: "ZODh7sdOOfI"
-    },
-    {
-
-        heading: "Image to text converter",
-        overview: "It converts the image to text with tenserflow library",
-        features: [
-            "convert image to text",
-            "innovative",
-            "fast functionality",
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "axios",
-
-            "tessaract.js",
-
-        ],
-        liveSiteURL: 'https://satya-img-2-text.netlify.app',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/img-2-text-converter",
-        videoId: "2RDETgB0P2c"
-    },
-    {
-        heading: "The Movie App",
-        overview: "It displays movie data and helps to find relevant information",
-        features: [
-            "search functionality",
-            "data of aprox all  the movies in th world available",
-            "deep data available",
-            "good ui",
-
-        ],
-        techStacks: [
-            "react",
-            "react-router-dom",
-            "axios",
-            "fontawesome",
-            "styled components",
-
-        ],
-        liveSiteURL: 'https://the-movie-app-satya.netlify.app/',
-        sourceCodeURL: "https://github.com/satyaarthchhabra/the-movie-app/",
-        videoId: "aBH9KYSQro8"
-    },
-]
-export default projectData
+  {
+    heading: "King Screen",
+    overview:
+      "Your new homepage to improve your productivity Today kingscreen is live with its first version v1.0.0 beta.",
+    features: [
+      "Bookmark List",
+      "responsive design",
+      "Google search",
+      "Weather Report",
+      "Pomodoro",
+      "Audio Player",
+      "Notes",
+      "Todo List",
+      "Clock",
+      "Quotes",
+    ],
+    techStacks: [
+      "React v17.0.2",
+      "TypeScript",
+      "Styled Components",
+      "React Icons",
+      "Netlify Hosting",
+    ],
+    liveSiteURL: "https://kingscreen.netlify.app/",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/kingscreen",
+    videoId: "-HUP-3ni4IY",
+  },
+  {
+    heading: "Ecommerce Amazon",
+    overview:
+      "ecommerce site with all neccessary features like admin dashboard and seller dashboard having backed with firebase and payments through stripe",
+    features: [
+      "multi paged website",
+      "responsive design",
+      "stripe payments",
+      "login functionality",
+      "login with google",
+      "automatic city and state detection system",
+      "profile upgardation ",
+    ],
+    techStacks: [
+      "react",
+      "react-router-dom",
+      "material-ui",
+      "firebase",
+      "stripe.js",
+      "axios",
+      "faker",
+      "moment",
+      "react-currency-format",
+      "react-flip-move",
+      "uuid",
+      "toastify",
+    ],
+    liveSiteURL: "https://satya-amazon-clone.netlify.app",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/amazon-clone-firebase",
+    videoId: "MthDpsxcDGc",
+  },
+  {
+    heading: "Netflix Clone",
+    overview:
+      "Built with react, this site is an media streaming site with backend in firebase  ",
+    features: [
+      "multi paged website",
+      "responsive design",
+      "search functionality",
+      "login functionality",
+      "login with google",
+      "youtube-video for the content will be played ",
+    ],
+    techStacks: [
+      "react",
+      "react-router-dom",
+      "react-youtube",
+      "firebase",
+      "normalize.css",
+      "fuse.js",
+      "styled-components",
+    ],
+    liveSiteURL: "https://satya-netflix-clone.netlify.app",
+    sourceCodeURL:
+      "https://github.com/satyaarthchhabra/netflix-clone-firebase-auth",
+    videoId: "mymuAhxSMN8",
+  },
+  {
+    heading: "World Corona Monitor",
+    overview:
+      "Displays covid-19 status in different countries through maps,graphs and lists",
+    features: [
+      "Daily covid cases",
+      "Map representations for cases",
+      "Graph representations for cases",
+      "Each Country alone representation also available",
+    ],
+    techStacks: [
+      "react",
+      "react-router-dom",
+      "axios",
+      "react-chartjs",
+      "leaflet",
+      "numeral",
+      "react-charjs-2",
+    ],
+    liveSiteURL: "https://satya-corona-go.netlify.app",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/go-corona/",
+    videoId: "Kkd8v80SsOg",
+  },
+  {
+    heading: "Meme Wala",
+    overview:
+      "It contains over 150 meme template and can help you to create meme",
+    features: [
+      "aprox 200 meme template",
+      "easy process",
+      "fast functionality",
+      "download functionality available ",
+    ],
+    techStacks: [
+      "react",
+      "react-router-dom",
+      "axios",
+      "react-bootstrap",
+      "bootstrap",
+    ],
+    liveSiteURL: "https://meme-wala.netlify.app",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/meme-wala/",
+    videoId: "ZODh7sdOOfI",
+  },
+  {
+    heading: "Image to text converter",
+    overview: "It converts the image to text with tenserflow library",
+    features: ["convert image to text", "innovative", "fast functionality"],
+    techStacks: ["react", "react-router-dom", "axios", "tessaract.js"],
+    liveSiteURL: "https://satya-img-2-text.netlify.app",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/img-2-text-converter",
+    videoId: "2RDETgB0P2c",
+  },
+  {
+    heading: "The Movie App",
+    overview: "It displays movie data and helps to find relevant information",
+    features: [
+      "search functionality",
+      "data of aprox all  the movies in th world available",
+      "deep data available",
+      "good ui",
+    ],
+    techStacks: [
+      "react",
+      "react-router-dom",
+      "axios",
+      "fontawesome",
+      "styled components",
+    ],
+    liveSiteURL: "https://the-movie-app-satya.netlify.app/",
+    sourceCodeURL: "https://github.com/satyaarthchhabra/the-movie-app/",
+    videoId: "aBH9KYSQro8",
+  },
+];
+export default projectData;
